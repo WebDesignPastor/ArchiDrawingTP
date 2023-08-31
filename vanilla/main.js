16914fd1880ef3fc782517ee82ffb009322e887d
Francois PARMENTIER$ <webcreationtp@gmail.com>
2023-08-31 09:38:25 +0200

    Model successfully working

--- vanilla/main.js
+++ vanilla/main.js
@@ -1,24 +1,60 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { Scene, PerspectiveCamera, WebGLRenderer, PointLight } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GridHelper } from 'three';
+import { AxesHelper } from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+
+// Set up the scene
+const scene = new Scene();
+
+// Camera setup
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 30); // or some larger value to "zoom out"
+
+// Renderer setup
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Add a point light
+const light = new PointLight(0xFFFFFF, 1, 100);
+light.position.set(0, 0, 5);
+scene.add(light);
+
+// Load the GLB model
+const loader = new GLTFLoader();
+loader.load('/maison.glb',
+   (gltf) => {
+      console.log("Model loaded successfully.");
+      gltf.scene.scale.set(0.1, 0.1, 0.1);  // Adjust this accordingly
+      scene.add(gltf.scene);
+   },
+   undefined,
+   (error) => {
+      console.error("Error loading the model.", error);
+   }
+);
+
+const grid = new GridHelper(50, 50);
+scene.add(grid);
+
+const axes = new AxesHelper(5);
+scene.add(axes);
+
+
+// Animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
+
+// Handle window resizing
+window.addEventListener('resize', () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+});
